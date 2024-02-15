9c5fc1837e75469d09b49266b8e19d03b248cae6
dysarz11 <justindysarz01@gmail.com>
2024-02-15 17:02:14 -0600

    Make combat loop with placeholders

--- Main.java
+++ Main.java
@@ -1,24 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 //This program uses java 21.0.1
 public class Main {
     public static void main(String[] args) {
-        int hpTest;
-        int strTest;
-        int dexTest;
-        int aglTest;
-
+        Scanner kb = new Scanner(System.in);
+        Random rand = new Random();
         PlayerStats playerStats = new PlayerStats();
         EnemyStats enemyStats = new EnemyStats();
+        GameLoop gameLoop = new GameLoop();
 
         playerStats.setPlayerHP(20);
         playerStats.setPlayerSTR(3);
-        playerStats.setPlayerDEX(2);
+        playerStats.setPlayerDEX(3);
         playerStats.setPlayerAGL(4);
 
-        hpTest = playerStats.getPlayerHP();
-        strTest = playerStats.getPlayerSTR();
-        dexTest = playerStats.getPlayerDEX();
-        aglTest = playerStats.getPlayerAGL();
+        enemyStats.setEnemyHP(30);
+        enemyStats.setEnemySTR(5);
+        enemyStats.setEnemyDEX(2);
+        enemyStats.setEnemyAGL(1);
 
-        System.out.println(hpTest + "\n" + strTest + "\n" + dexTest + "\n" + aglTest);
+        gameLoop.combatLoop(playerStats, enemyStats);
     }
 }
