9a324de4b1003ceef499920e66cf7fb490a138ce
dysarz11 <justindysarz01@gmail.com>
2024-02-15 16:39:44 -0600

    Make playerStats class make getters and setters

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        int hpTest;
+        int strTest;
+        int dexTest;
+        int aglTest;
+
+        PlayerStats playerStats = new PlayerStats();
+
+        playerStats.setPlayerHP(20);
+        playerStats.setPlayerSTR(3);
+        playerStats.setPlayerDEX(2);
+        playerStats.setPlayerAGL(4);
+
+        hpTest = playerStats.getPlayerHP();
+        strTest = playerStats.getPlayerSTR();
+        dexTest = playerStats.getPlayerDEX();
+        aglTest = playerStats.getPlayerAGL();
+        System.out.println(hpTest + "\n" + strTest + "\n" + dexTest + "\n" + aglTest);
     }
 }
---
+++ PlayerStats.java
@@ -0,0 +1,40 @@
+public class PlayerStats {
+    private int playerHP;
+    private int playerSTR;
+    private int playerDEX;
+    private int playerAGL;
+
+    public void setPlayerHP(int hp) {
+        playerHP = hp;
+    }
+
+    public int getPlayerHP() {
+        return  playerHP;
+    }
+
+    public void setPlayerSTR(int str) {
+        playerSTR = str;
+    }
+
+    public int getPlayerSTR() {
+        return  playerSTR;
+    }
+
+    public void setPlayerDEX(int dex) {
+        playerDEX = dex;
+    }
+
+    public int getPlayerDEX() {
+        return  playerDEX;
+    }
+
+    public void setPlayerAGL(int agl) {
+        playerAGL = agl;
+    }
+
+    public int getPlayerAGL() {
+        return  playerAGL;
+    }
+
+
+}
