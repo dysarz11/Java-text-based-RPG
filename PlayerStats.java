78fb4b69ffc9766e94fb825db753234f9652341f
dysarz11 <justindysarz01@gmail.com>
2024-02-16 13:00:13 -0600

    Add more player stats Add default constructor

--- PlayerStats.java
+++ PlayerStats.java
@@ -1,8 +1,23 @@
 public class PlayerStats {
     private int playerHP;
+    private int playerDefense;
     private int playerSTR;
     private int playerDEX;
     private int playerAGL;
+    private int playerINT;
+    private int playerFTH;
+    private int playerLuck;
+
+    public PlayerStats() {
+        playerHP = -1;
+        playerDefense = -1;
+        playerSTR = -1;
+        playerDEX = -1;
+        playerAGL = -1;
+        playerINT = -1;
+        playerFTH = -1;
+        playerLuck = -1;
+    }
 
     public void setPlayerHP(int hp) {
         playerHP = hp;
