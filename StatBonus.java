0a350c24bdc11e10fa19c830de6d65f8466cf154
Justin <justindysarz01@gmail.com>
2024-02-18 10:21:30 -0500

    make stat bonus class

---
+++ StatBonus.java
@@ -0,0 +1,12 @@
+public class StatBonus {
+	private STRBonus;
+	private DEXBonus;
+	private AGLBonus;
+	private INTBonus;
+	private FTHBonus;
+	private LUCKBonus;
+
+	public void setSTRBonus(PlayerStats playerStats) {
+
+	}
+}
